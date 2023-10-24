740dffd2e3e578c9ad1ee87f7c9bc9c8f83866eb
Kaushik, Arpit <kaushi@deshaw.com>
2023-10-24 17:45:04 +0530

    Design HashMap

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -80,7 +80,7 @@ public class MyHashMap<K,V> {
         map.put(2, "my");
         map.put(3, "name");
         map.put(4, "is");
-        map.put(5, "Shrayansh");
+        map.put(5, "Arpit");
         map.put(6, "how");
         map.put(7, "are");
         map.put(8, "you");
